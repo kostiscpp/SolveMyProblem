149743193936ba0745cbece7c1599a1f99a95be5
ntua-el20103 <el20103@mail.ntua.gr>
2024-09-14 10:00:19 +0300

    run_services_linux.sh improved

--- run_services_linux.sh
+++ run_services_linux.sh
@@ -5,9 +5,9 @@ sudo systemctl start rabbitmq-server
 sudo systemctl start mongod
 
 # Run each service in a new terminal window with a specific title
-gnome-terminal --title="Orchestrator Service" -- bash -c "cd orchestrator && node app.js; exec bash"
-gnome-terminal --title="User Service" -- bash -c "cd user-service && node app.js; exec bash"
-gnome-terminal --title="Problem Management Service" -- bash -c "cd problem-management-service && node app.js; exec bash"
-gnome-terminal --title="Transaction Service" -- bash -c "cd transaction-service && node app.js; exec bash"
-gnome-terminal --title="Solver Service" -- bash -c "cd solver-service && node solver.js; exec bash"
-gnome-terminal --title="Frontend" -- bash -c "cd frontend/fe && export NODE_OPTIONS=--openssl-legacy-provider && npm start; exec bash"
+gnome-terminal --tab --title="Orchestrator Service" -- bash -c "cd orchestrator && node app.js; exec bash"
+gnome-terminal --tab --title="User Service" -- bash -c "cd user-service && node app.js; exec bash"
+gnome-terminal --tab --title="Problem Management Service" -- bash -c "cd problem-management-service && node app.js; exec bash"
+gnome-terminal --tab --title="Transaction Service" -- bash -c "cd transaction-service && node app.js; exec bash"
+gnome-terminal --tab --title="Solver Service" -- bash -c "cd solver-service && node solver.js; exec bash"
+gnome-terminal --tab --title="Frontend" -- bash -c "cd frontend/fe && export NODE_OPTIONS=--openssl-legacy-provider && npm start; exec bash"
